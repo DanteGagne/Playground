b6eab540770501e8b28bb5464d2b1caeaf9d36ac
Dante Gagne <danteg@microsoft.com>
2021-02-03 10:03:50 -0800

    Added .editorconfig

--- NetCoreConsolePlayground/Program.cs
+++ NetCoreConsolePlayground/Program.cs
@@ -4,9 +4,23 @@ namespace NetCoreConsolePlayground
 {
 	class Program
 	{
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="args"></param>
 		static void Main(string[] args)
 		{
+			throw new NotImplementedException();
 			Console.WriteLine("Hello World!");
+			if (false)
+			{
+				Console.WriteLine("fail");
+			}
 		}
+
+		public void iWannaFail()
+        {
+
+        }
 	}
 }
